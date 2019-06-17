298b4509bef0a909b80d6b06f09902920ead0b36
Umair Baig <ubaig@MacBook-Pro.local>
2019-06-17 16:11:30 -0400

    initialized npm, and added validator node package

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-const getNotes = require('./notes');
+const validator = require('validator'), 
+getNotes = require('./notes');
 
 console.log(getNotes());
+
+console.log(validator.isEmail('food.com'));
