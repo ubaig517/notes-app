b10bed90feb5cc086705fe1e1dfac66d8ae9bca9
Umair Baig <ubaig@MacBook-Pro.local>
2019-06-19 16:38:17 -0400

    install yargs and set up initial command line commands

--- app.js
+++ app.js
@@ -1,6 +1,46 @@
-const validator = require('validator'), 
+const chalk = require('chalk'),
+yargs = require('yargs'), 
 getNotes = require('./notes');
 
-console.log(getNotes());
+// customize yargs version
+yargs.version('1.1.0');
 
-console.log(validator.isEmail('food.com'));
+// add, remove, read, list Command config
+
+// create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('adding a new note');
+    }
+})
+
+// create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('removing a note');
+    }
+})
+
+// create list command
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: function() {
+        console.log('Listing all your notes');
+    }
+})
+
+// create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read all notes',
+    handler: function() {
+        console.log('Reading a note');
+    }
+})
+
+console.log(yargs.argv);
