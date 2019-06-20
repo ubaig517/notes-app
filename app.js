49bdd5af7988c468566a07a267a65fe7307e726b
Umair Baig <ubaig@MacBook-Pro.local>
2019-06-20 17:07:25 -0400

    Set up the builder object for the Add command

--- app.js
+++ app.js
@@ -11,8 +11,21 @@ yargs.version('1.1.0');
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function() {
-        console.log('adding a new note');
+    builder: {
+        title:{
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log('Title: ', argv.title);
+        console.log('Body: ', argv.body);
     }
 })
 
@@ -24,7 +37,6 @@ yargs.command({
         console.log('removing a note');
     }
 })
-
 // create list command
 yargs.command({
     command: 'list',
@@ -43,4 +55,5 @@ yargs.command({
     }
 })
 
-console.log(yargs.argv);
+yargs.parse();
+// console.log(yargs.argv);
